00706e21f6004c33991d1f37ecb253966941c1c2
janardhana <johnnadupuru186@gmail.com>
2023-08-07 14:06:03 +0530

    bg change

--- src/components/CommunityHall/CommunityHall.tsx
+++ src/components/CommunityHall/CommunityHall.tsx
@@ -5,7 +5,7 @@ const CommunityHall = () => {
   return (
     <section className="px-3 lg:px-[50px] mt-3">
       <div>
-        <div className=" lg:h-[650px] bg-cover bg-center bg-[url(community/communitybg.jpg)] rounded-xl">
+        <div className=" lg:h-[650px] bg-cover bg-center rounded-xl" style={{backgroundImage:"url(community/communitybg.jpg)"}}>
           <div className="bg-black/80 h-full left-0 right-0 top-0 bottom-0 rounded-xl p-5">
             <div className=" ">
               <h1 className="text-white font-bold md:text-5xl text-3xl">Village Community Hall</h1>
@@ -32,8 +32,7 @@ const CommunityHall = () => {
                   <h1 className="text-white text-3xl font-bold">Community Hall</h1>
                   <p className="text-white font-semibold text-xl">A village community hall holds significant importance as a central gathering place for residents in a rural setting. It serves as a focal point for social, cultural, and developmental activities, contributing to the overall well-being and progress of the village.
                   A community hall provides a space where villagers can come together, fostering a sense of unity, belonging, and social cohesion. It strengthens relationships among neighbors, enhances community bonding, and reduces social isolation.
-                  Community halls can be used for educational purposes, such as conducting workshops, seminars, and training sessions.Satyadeep Foundation hope to will build like these type of community hall in village. At that village poor peoples use for their needs. And it will expands to establish like library. It will helps meet their peoples at on place to share their things.Now we find our happiness in other people's smile.
-                  Your generous donation will go directly towards funding the construction of the Village Community Hall.</p>
+                  Community halls can be used for educational purposes, such as conducting workshops, seminars, and training sessions.Satyadeep Foundation hope to will build like these type of community hall in village. At that village poor peoples use for their needs. And it will expands to establish like library. It will helps meet their peoples at on place to share their things.</p>
                   <div className="flex justify-center mt-5 mb-5">
                     <button onClick={()=>navigate("/payments")} className="text-white cursor-pointer p-2 rounded-lg text-center shadow-xl bg-orange-800 hover:bg-red-900">Join With Us</button>
                   </div>
